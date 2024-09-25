46d656d5fc179f5f813ec4d9eda8695c4e50be24
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-09-26 01:36:10 +0300

    setup services section

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -1,7 +1,22 @@
 import styles from "./Services.module.css";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className={styles["services-container"]}>
+      <div className={styles["package-title-container"]}>
+        <div>
+          <p>Tour Packages</p>
+          <h2>Our tourist destination</h2>
+        </div>
+        <p>
+          Our tourist destinations offer an unrivaled blend of natural beauty
+          and cultural richness.
+        </p>
+      </div>
+      <div></div>
+      <div></div>
+    </section>
+  );
 };
 
 export default Services;
