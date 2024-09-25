78dcd59e6a2f8f84b4a50f82ba4ccb8ceaff0e58
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-09-26 00:25:56 +0300

    enhance location images

--- src/pages/location/Location.tsx
+++ src/pages/location/Location.tsx
@@ -14,21 +14,33 @@ const Location = () => {
         </p>
       </div>
       <div className={styles["location-image-container"]}>
-        <div className={styles["location-image-subcontainer"]}>
+        <div className={styles["location-image-subcontainer1"]}>
           <img src="./pool.jpg" alt="" className={styles["location-imageG1"]} />
           <img
             src="./mountain.jpg"
             alt=""
             className={styles["location-imageG2"]}
           />
+          <p className={styles["location-imageDesc1"]}>
+            Bromo East, Java <br /> <b>Bromo Tengger Tour</b>
+          </p>
+          <p className={styles["location-imageDesc2"]}>
+            Denpasar, Bali <br /> <b>Bali Beach Tourism</b>
+          </p>
         </div>
-        <div className={styles["location-image-subcontainer"]}>
+        <div className={styles["location-image-subcontainer2"]}>
           <img
             src="./waterfall.jpg"
             alt=""
             className={styles["location-imageG2"]}
           />
           <img src="./car.jpg" alt="" className={styles["location-imageG1"]} />
+          <p className={styles["location-imageDesc3"]}>
+            Bromo East, Java <br /> <b>Bromo Tengger Tour</b>
+          </p>
+          <p className={styles["location-imageDesc4"]}>
+            Denpasar, Bali <br /> <b>Bali Beach Tourism</b>
+          </p>
         </div>
       </div>
     </section>
