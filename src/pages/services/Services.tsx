af2e7bf51c5ca284c823f4a5b09170e1368103c0
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-09-30 13:27:18 +0300

    complete services section

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -13,8 +13,45 @@ const Services = () => {
           and cultural richness.
         </p>
       </div>
+      <div className={styles["package-img-container"]}>
+        <div className={styles["package-img1container"]}>
+          <img src="./trees.jpg" alt="" className={styles["package-img1"]} />
+          <div className={styles["date-price-cridentials-container1"]}>
+            <p>23 AUGUST - 29 AUGUST</p>
+            <div className={styles["package-price-container"]}>
+              <p>Bail Tour Package</p>
+              <p>$285</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles["package-img2container"]}>
+          <img
+            src="./building1.jpg"
+            alt=""
+            className={styles["package-img1"]}
+          />
+          <div className={styles["date-price-cridentials-container2"]}>
+            <p>23 AUGUST - 29 AUGUST</p>
+            <div className={styles["package-price-container"]}>
+              <p>Bail Tour Package</p>
+              <p>$218</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles["package-img3container"]}>
+          <img src="./game.jpg" alt="" className={styles["package-img1"]} />
+          <div className={styles["date-price-cridentials-container3"]}>
+            <p>23 AUGUST - 29 AUGUST</p>
+            <div className={styles["package-price-container"]}>
+              <p>Bail Tour Package</p>
+              <p>$163</p>
+            </div>
+          </div>
+        </div>
+        <span></span>
+      </div>
       <div></div>
-      <div></div>
+      <button className={styles["view-more-btn"]}>View more</button>
     </section>
   );
 };
