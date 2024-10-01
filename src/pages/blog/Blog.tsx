8a97fb5f93219d9b91f3fe480bd55f700ae1fb07
kennedy-muhumuza <muhumuzakennedy17@gmail.com>
2024-10-01 20:02:24 +0300

    add blog section

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -8,7 +8,37 @@ const Blog = () => {
           <p>Our Blog</p>
           <h2>Our travel memories</h2>
         </div>
-        <div></div>
+        <div className={styles["card-container"]}>
+          <div className={styles["card"]}>
+            <img src="./car.jpg" alt="" className={styles["card-img1"]} />
+            <div className={styles["card-details"]}>
+              <p className={styles["card-date"]}>Nov 14, 2022</p>
+              <span>
+                <b>2023 Travel Trends - what you need to know</b>
+              </span>
+              <p>
+                2023 taught taught us valuable life lessons. Plans don't always
+                work out, flexibility in life is key.
+              </p>
+            </div>
+          </div>
+          <div className={styles["card"]}>
+            <img src="./boat.jpg" alt="" className={styles["card-img1"]} />
+            <div className={styles["card-details"]}>
+              <p className={styles["card-date"]}>Nov 18, 2022</p>
+              <span>
+                <b>2023 Travel Trends - what you need to know</b>
+              </span>
+              <p>
+                2023 taught taught us valuable life lessons. Plans don't always
+                work out, flexibility in life is key.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles["btn-container"]}>
+          <button className={styles["view-more-btn"]}>View more</button>
+        </div>
       </div>
     </section>
   );
